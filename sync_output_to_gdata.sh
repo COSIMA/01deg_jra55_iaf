58befebc4d64004fb5367470431e3fa6248f24d3
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2017-11-23 16:36:18 +1100

    update

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -8,7 +8,7 @@
 
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # (make a unique path for your set of runs)
-GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf9091_spinup1
+GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf8485_spinup1
 
 mkdir -p ${GDATADIR}
 cd archive
