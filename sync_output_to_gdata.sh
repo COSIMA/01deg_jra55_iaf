8d6d62eafffad3b65cea3fb3bb8d3e01997c4c80
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2017-11-25 13:42:15 +1100

    as used in first run

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -4,11 +4,11 @@
 #PBS -l wd
 #PBS -l walltime=1:00:00,mem=2GB
 #PBS -P v45
-#PBS -N sync_to_gdata
+#PBS -N sync_output_to_gdata
 
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # (make a unique path for your set of runs)
-GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf8485_spinup1
+GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf8485_spinup2
 
 mkdir -p ${GDATADIR}
 cd archive
