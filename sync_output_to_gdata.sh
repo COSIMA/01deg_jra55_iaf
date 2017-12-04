1c27de814385d79262be281b79d57db76fd2fc66
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2017-12-04 13:45:50 +1100

    sync_restarts_to_gdata.sh now sycs all restarts

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -4,7 +4,7 @@
 #PBS -l wd
 #PBS -l walltime=1:00:00,mem=2GB
 #PBS -P v45
-#PBS -N sync_to_gdata
+#PBS -N sync_output_to_gdata
 
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # (make a unique path for your set of runs)
--- sync_restarts_to_gdata.sh
+++ sync_restarts_to_gdata.sh
@@ -8,4 +8,5 @@
 
 source sync_output_to_gdata.sh # to define GDATADIR and cd archive
 
-rsync -av --safe-links --no-g restart??[05]* ${GDATADIR}
+# rsync -av --safe-links --no-g restart??[05]* ${GDATADIR}
+rsync -av --safe-links --no-g restart* ${GDATADIR}
