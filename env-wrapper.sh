73058bffa962fc47adbdeaa29014116ea5183c52
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2018-05-17 17:54:13 +1000

    Trying ndtd=2 and 2700cpus for CICE

--- env-wrapper.sh
+++ env-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-log=/short/$PROJECT/$USER/mom/tmp/${PBS_JOBID}
+log=$(pwd)/archive/env-wrapper_logs/${PBS_JOBID}
 mkdir -p ${log}
 world_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_RANK})
 local_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_LOCAL_RANK})
