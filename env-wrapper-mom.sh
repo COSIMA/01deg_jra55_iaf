70f2e2ae5547a7188d784258d8a0316d38ba14f0
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2018-07-27 12:27:05 +1000

    Made env-wrappers for all 3 executables

--- env-wrapper-mom.sh
+++ env-wrapper-mom.sh
@@ -3,6 +3,6 @@ log=$(pwd)/archive/env-wrapper_logs/${PBS_JOBID}
 mkdir -p ${log}
 world_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_RANK})
 local_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_LOCAL_RANK})
-envdump=environ-${PBS_JOBID}.${world_rank}-$(hostname).${local_rank}
+envdump=environ-mom-${PBS_JOBID}.${world_rank}-$(hostname).${local_rank}
 env > ${log}/${envdump}
-exec "$@"
+exec /short/v45/aek156/CHUCKABLE/OceansAus/access-om2-yatm/access-om2/bin/fms_ACCESS-OM_bae32b6_libaccessom2_84acdb8.x "$@"
--- env-wrapper-yatm.sh
+++ env-wrapper-yatm.sh
@@ -3,6 +3,6 @@ log=$(pwd)/archive/env-wrapper_logs/${PBS_JOBID}
 mkdir -p ${log}
 world_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_RANK})
 local_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_LOCAL_RANK})
-envdump=environ-${PBS_JOBID}.${world_rank}-$(hostname).${local_rank}
+envdump=environ-yatm-${PBS_JOBID}.${world_rank}-$(hostname).${local_rank}
 env > ${log}/${envdump}
-exec "$@"
+exec /short/v45/aek156/CHUCKABLE/OceansAus/access-om2-yatm/access-om2/bin/yatm_84acdb8.exe "$@"
