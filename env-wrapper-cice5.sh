d3c343ec6b01e4f68fb09d01373cbba74455dfa1
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2018-08-02 21:52:19 +1000

    Try halving ncpus for mom and cice

--- env-wrapper-cice5.sh
+++ env-wrapper-cice5.sh
@@ -6,4 +6,5 @@ local_rank=$(printf "%05d\n" ${OMPI_COMM_WORLD_LOCAL_RANK})
 envdump=environ-cice5-${PBS_JOBID}.${world_rank}-$(hostname).${local_rank}
 env > ${log}/${envdump}
 # exec /short/v45/aek156/CHUCKABLE/OceansAus/access-om2-yatm/access-om2/bin/cice_auscom_3600x2700_2000p_a3391cb_libaccessom2_84acdb8.exe "$@"
-exec   /short/v45/aek156/CHUCKABLE/OceansAus/access-om2-yatm/access-om2/bin/cice_auscom_3600x2700_2000p_0981e59_libaccessom2_af01d11.exe "$@"
+# exec /short/v45/aek156/CHUCKABLE/OceansAus/access-om2-yatm/access-om2/bin/cice_auscom_3600x2700_2000p_0981e59_libaccessom2_af01d11.exe "$@"
+exec  /short/v45/aek156/CHUCKABLE/OceansAus/access-om2-yatm/access-om2/bin/cice_auscom_3600x2700_1000p_63a20ac_libaccessom2_af01d11.exe "$@"
