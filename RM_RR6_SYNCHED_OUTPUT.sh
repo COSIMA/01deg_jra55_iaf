3fd6cb5b4e0967521647214a0db1b1ebebcc51b8
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2018-09-12 09:59:37 +1000

    Added RM_RR6_SYNCHED_OUTPUT.sh

---
+++ RM_RR6_SYNCHED_OUTPUT.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -q copyq
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -l walltime=4:00:00,mem=2GB
+#PBS -P v45
+#PBS -N RM_RR6_SYNCHED_OUTPUT
+
+source sync_output_to_rr6.sh # to define GDATARR6DIR and cd archive
+echo
+echo "======================================================="
+echo
+
+rsync --dry-run --remove-source-files --include '*/' --include 'ocean_daily_3d_*.nc' --exclude '*' --exclude "*.nc.*" --exclude "*ocean_*_3hourly*" --exclude "*iceh_03h*" -vrltoD --safe-links output001 ${GDATARR6DIR}
