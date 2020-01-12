48927b8f2d35c4bfd3434d1947f73d11cba6513b
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-01-13 10:13:28 +1100

    sync_output_to_gdata.sh: set PBS flag for storage on gadi; exit if paths inaccessible

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -4,6 +4,7 @@
 #PBS -l wd
 #PBS -l walltime=4:00:00,mem=4GB
 #PBS -P v45
+#PBS -l storage=gdata/hh5+scratch/v45
 #PBS -N output_to_gdata
 
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
@@ -11,8 +12,8 @@
 # DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
 GDATADIR=/ERROR/SET/GDATADIR/IN/sync_output_to_gdata.sh
 
-mkdir -p ${GDATADIR}
-cd archive
+mkdir -p ${GDATADIR} || exit 1
+cd archive || exit 1
 rsync --exclude "*.nc.*" --exclude "ocean_daily_3d_*" --exclude "*ocean_*_3hourly*" --exclude "*iceh_03h*" -vrltoD --safe-links output* ${GDATADIR}
 rsync -vrltoD --safe-links error_logs ${GDATADIR}
 rsync -vrltoD --safe-links pbs_logs ${GDATADIR}
