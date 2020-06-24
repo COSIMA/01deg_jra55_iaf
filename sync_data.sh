dea36e483e6debd8cbe738103169e133025bf391
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-06-24 10:34:27 +1000

    improve sync_data.sh

--- sync_data.sh
+++ sync_data.sh
@@ -3,7 +3,7 @@
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -l walltime=4:00:00,mem=4GB
-#PBS -l storage=gdata/hh5+gdata/ik11+scratch/v45+scratch/x77
+#PBS -l storage=gdata/hh5+gdata/ik11+scratch/v45+scratch/x77+scratch/g40
 #PBS -N sync
 
 # Set SYNCDIR to the path you want your data copied to.
@@ -16,6 +16,7 @@ SYNCDIR=/g/data/ik11/outputs/access-om2-01/01deg_jra55v140_iaf
 exitcode=0
 help=false
 dirtype=output
+exclude="--exclude *.nc.*"
 rmlocal=false
 
 # parse argument list
@@ -27,6 +28,10 @@ while [ $# -ge 1 ]; do
         -r)
             echo "syncing restarts instead of output directories"
             dirtype=restart
+            ;;
+        -u)
+            echo "syncing collated and uncollated .nc files"
+            exclude=""
             ;;
         -D)
         # --remove-source-files tells rsync to remove from the sending side the files (meaning non-directories) 
@@ -43,12 +48,10 @@ while [ $# -ge 1 ]; do
             ;;
         -*)
             echo $1": invalid option"
-            help=true
             exitcode=1
             ;;
         *)
             echo $1": invalid argument"
-            help=true
             exitcode=1
             ;;
     esac
@@ -56,33 +59,34 @@ while [ $# -ge 1 ]; do
 done
 
 if [ $exitcode != "0" -o $help == true ]; then
-    echo $0": rsync model run outputs (and optionally restarts) to another location."
+    echo "$0: rsync model run outputs (and optionally restarts) to another location."
     echo "  Must be invoked from a control directory."
-    echo "  "$0" should be edited to set SYNCDIR."
+    echo "  $0 should be edited to set SYNCDIR."
     echo "  Default will rsync all output directories, leaving local copies intact."
-    echo "  Uncollated .nc files are not synced."
+    echo "  Uncollated .nc files are not rsynced unless the -u option is used."
     echo "  Also rsyncs error_logs and pbs_logs."
     echo "  Also updates git-runlog, a git clone of the control directory (whose git history documents all changes in the run)."
     echo "  Also updates, rsyncs and commits run summary"
-    echo "usage: "$0" [-h] [-r] [-D]"
+    echo "usage: $0 [-h] [-r] [-u] [-D]"
     echo "  -h: show this help message and exit"
     echo "  -r: sync all restart directories instead of output directories"
+    echo "  -u: sync collated and uncollated .nc files (default is collated only)"
     echo "  -D: delete local copies of synced files in all but the most recent synced directories (outputs or restarts, depending on -r). Must be done interactively. (Default leaves local copies intact.)"
     exit $exitcode
 fi
 
 sourcepath="$PWD"
-mkdir -p ${SYNCDIR} || exit 1
+mkdir -p ${SYNCDIR} || { echo "Error: cannot create ${SYNCDIR} - edit $0 to set SYNCDIR"; exit 1; }
 cd archive || exit 1
 
 # first delete any cice log files that only have a 105-character header and nothing else
 find output* -size 105c -iname "ice.log.task_*" -delete
 
 # copy all collated outputs/restarts
-rsync --exclude "*.nc.*" -vrltoD --safe-links $dirtype[0-9][0-9][0-9] ${SYNCDIR}
+rsync $exclude -vrltoD --safe-links $dirtype[0-9][0-9][0-9] ${SYNCDIR}
 if [ $rmlocal == true ]; then
     # Now do removals. Don't remove final local copy, so we can continue run.
-    rsync --remove-source-files --exclude `\ls -1d $dirtype[0-9][0-9][0-9] | tail -1` --exclude "*.nc.*" -vrltoD --safe-links $dirtype[0-9][0-9][0-9] ${SYNCDIR}
+    rsync --remove-source-files --exclude `\ls -1d $dirtype[0-9][0-9][0-9] | tail -1` $exclude -vrltoD --safe-links $dirtype[0-9][0-9][0-9] ${SYNCDIR}
 fi
 
 # Also sync error and PBS logs
@@ -106,4 +110,4 @@ git add run_summary*.csv
 git commit -m "update run summary"
 cd ${SYNCDIR}/git-runlog && git pull --no-rebase
 
-echo $0" completed successfully"
+echo "$0 completed successfully"
