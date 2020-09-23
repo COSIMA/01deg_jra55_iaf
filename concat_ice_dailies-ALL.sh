04b9d3f0abc041ee6d30cf478cc48c47611fb375
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-09-23 13:40:49 +1000

    update concat_ice_dailies*.sh

--- concat_ice_dailies-ALL.sh
+++ concat_ice_dailies-ALL.sh
@@ -11,12 +11,12 @@ case $yesno in
     * ) echo "Cancelled. Wait until model has finished before trying again."; exit 0;;
 esac
 
-for d in /g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf8485_spinup6/output???/ice/OUTPUT; do
+for d in /g/data/ik11/outputs/access-om2-01/01deg_jra55v140_iaf/output???/ice/OUTPUT; do
     for f in $d/iceh.????-??.nc; do
-        if [[ -f ${f/.nc/-01.nc} ]] && [[ ! -f ${f/.nc/-IN-PROGRESS} ]];
+        if [[ -f ${f/.nc/-01.nc} ]] && [[ ! -f ${f/.nc/-IN-PROGRESS} ]] && [[ ! -f ${f/.nc/-daily.nc} ]];
         then
             echo "Submitting $f"
-            qsub -P v45 -q copyq -l ncpus=1 -l walltime=00:30:00,mem=2GB -l wd -V -N concat_ice_dailies -- ./concat_ice_dailies.sh $f && touch ${f/.nc/-IN-PROGRESS}
+            qsub -P x77 -q copyq -l ncpus=1 -l walltime=00:30:00,mem=2GB -l wd -l storage=gdata/hh5+gdata/ik11+scratch/v45+scratch/x77+scratch/g40 -V -N concat_ice_dailies -- ./concat_ice_dailies.sh $f && touch ${f/.nc/-IN-PROGRESS}
         else
             echo "--- Skipping $f"
         fi
--- concat_ice_dailies.sh
+++ concat_ice_dailies.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 module load nco
 
-echo "doing ncrcat -O -L 5 -7 ${1/.nc/-??.nc} ${1/.nc/-daily.nc} && rm ${1/.nc/-??.nc}"
-# ncrcat -O -L 5 -7 ${1/.nc/-??.nc} ${1/.nc/-daily.nc}
-ncrcat -O -L 5 -7 ${1/.nc/-??.nc} ${1/.nc/-daily.nc} && rm ${1/.nc/-??.nc}
-chgrp v45 ${1/.nc/-daily.nc}
+echo "doing ncrcat -O -L 5 -7 ${1/.nc/-??.nc} ${1/.nc/-daily.nc}"
+ncrcat -O -L 5 -7 ${1/.nc/-??.nc} ${1/.nc/-daily.nc}
+# echo "doing ncrcat -O -L 5 -7 ${1/.nc/-??.nc} ${1/.nc/-daily.nc} && rm ${1/.nc/-??.nc}"
+# ncrcat -O -L 5 -7 ${1/.nc/-??.nc} ${1/.nc/-daily.nc} && rm ${1/.nc/-??.nc}
+chgrp ik11 ${1/.nc/-daily.nc}
 chmod g+r ${1/.nc/-daily.nc}
 rm ${1/.nc/-IN-PROGRESS}
