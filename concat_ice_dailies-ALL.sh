f76a7b7a8a8df32a8a48f4b4eea289c750130fb2
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-09-23 10:28:12 +1000

    add concat_ice_dailies*.sh from /home/156/aek156/raijin_home/aek156/payu/01deg_jra55v13_ryf8485_spinup6/

---
+++ concat_ice_dailies-ALL.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Concatenate and compress CICE daily output files
+
+# DO NOT USE WHILE MODEL IS RUNNING! This could concatenate and delete a partial set of daily files.
+
+echo "WARNING: do not proceed if the model is still running."
+read -p "Proceed? (y/n) " yesno
+case $yesno in
+    [Yy] ) ;;
+    * ) echo "Cancelled. Wait until model has finished before trying again."; exit 0;;
+esac
+
+for d in /g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf8485_spinup6/output???/ice/OUTPUT; do
+    for f in $d/iceh.????-??.nc; do
+        if [[ -f ${f/.nc/-01.nc} ]] && [[ ! -f ${f/.nc/-IN-PROGRESS} ]];
+        then
+            echo "Submitting $f"
+            qsub -P v45 -q copyq -l ncpus=1 -l walltime=00:30:00,mem=2GB -l wd -V -N concat_ice_dailies -- ./concat_ice_dailies.sh $f && touch ${f/.nc/-IN-PROGRESS}
+        else
+            echo "--- Skipping $f"
+        fi
+    done
+done
+
