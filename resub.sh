395318a72cfd0a8ad17cb20a922c0396908d7281
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-12-16 08:28:22 +1100

    switch to x77; MAX_RESUBMISSIONS=1

--- resub.sh
+++ resub.sh
@@ -4,7 +4,7 @@ logfile='resubmit.log'
 counterfile='resubmit.count'
 outfile='access-om2.err'
 
-MAX_RESUBMISSIONS=6
+MAX_RESUBMISSIONS=1
 date >> ${logfile}
 
 # Define errors from which a resubmit is appropriate
