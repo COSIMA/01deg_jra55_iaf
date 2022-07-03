76940542e836519b62a4c1f20b4f2570f9c9f4a9
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2022-07-03 10:34:29 +1000

    fix concat_ice_3hourlies* to concatenate 3 hourly data

--- concat_ice_3hourlies-ALL.sh
+++ concat_ice_3hourlies-ALL.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Concatenate and compress CICE 6hourly output files
+# Concatenate and compress CICE 3hourly output files
 
-# DO NOT USE WHILE MODEL IS RUNNING! This could concatenate and delete a partial set of 6hourly files.
+# DO NOT USE WHILE MODEL IS RUNNING! This could concatenate and delete a partial set of 3hourly files.
 
 shopt -s nullglob
 
@@ -14,11 +14,11 @@ case $yesno in
 esac
 
 for d in archive/output???/ice/OUTPUT; do
-    for f in $d/iceh*.????-??-01-64800.nc; do
-        if [[ ! -f ${f/.nc/-IN-PROGRESS} ]] && [[ ! -f ${f/-01-64800.nc/-6hourly.nc} ]];
+    for f in $d/iceh*.????-??-01-75600.nc; do
+        if [[ ! -f ${f/.nc/-IN-PROGRESS} ]] && [[ ! -f ${f/-01-75600.nc/-3hourly.nc} ]];
         then
             echo "Submitting $f"
-            qsub -P x77 -q copyq -l ncpus=1 -l walltime=01:30:00,mem=2GB -l wd -l storage=gdata/hh5+gdata/ik11+scratch/v45+scratch/x77+scratch/g40 -V -N concat_ice_6hourlies -- ./concat_ice_6hourlies.sh $f && touch ${f/.nc/-IN-PROGRESS}
+            qsub -P x77 -q copyq -l ncpus=1 -l walltime=01:30:00,mem=2GB -l wd -l storage=gdata/hh5+gdata/ik11+scratch/v45+scratch/x77+scratch/g40 -V -N concat_ice_3hourlies -- ./concat_ice_3hourlies.sh $f && touch ${f/.nc/-IN-PROGRESS}
         else
             echo "--- Skipping $f"
         fi
@@ -26,4 +26,4 @@ for d in archive/output???/ice/OUTPUT; do
 done
 
 echo
-echo "After the qsub jobs have all successfully completed, you should run concat_ice_6hourlies_fix.sh"
+echo "After the qsub jobs have all successfully completed, you should run concat_ice_3hourlies_fix.sh"
--- concat_ice_3hourlies.sh
+++ concat_ice_3hourlies.sh
@@ -2,11 +2,11 @@
 module load nco
 set -e
 
-echo "doing ncrcat -O -L 5 -7 ${1/-01-64800.nc/-??-?????.nc} ${1/-01-64800.nc/-6hourly.nc}"
-ncrcat -O -L 5 -7 ${1/-01-64800.nc/-??-?????.nc} ${1/-01-64800.nc/-6hourly.nc}
-chgrp x77 ${1/-01-64800.nc/-6hourly.nc}
-chmod g+r ${1/-01-64800.nc/-6hourly.nc}
-for f in ${1/-01-64800.nc/-??-?????.nc}
+echo "doing ncrcat -O -L 5 -7 ${1/-01-75600.nc/-??-?????.nc} ${1/-01-75600.nc/-3hourly.nc}"
+ncrcat -O -L 5 -7 ${1/-01-75600.nc/-??-?????.nc} ${1/-01-75600.nc/-3hourly.nc}
+chgrp x77 ${1/-01-75600.nc/-3hourly.nc}
+chmod g+r ${1/-01-75600.nc/-3hourly.nc}
+for f in ${1/-01-75600.nc/-??-?????.nc}
 do
    mv $f $f-DELETE
 done
